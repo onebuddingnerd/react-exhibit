f5b412595f2d3ed7cd414be2ec90df416759a14f
Anupam Pokharel <avikrk88@dhcp-9-240-117-173.bnt-us.ibmmobiledemo.com>
2022-12-04 13:46:46 -0600

    first

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import MuiBottomNavigationAction from '@mui/material/BottomNavigationAction';
+import ArticleIcon from '@mui/icons-material/Article';
+import HourglassEmptyIcon from '@mui/icons-material/HourglassEmpty';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import CloudQueueIcon from '@mui/icons-material/CloudQueue';
+import RenderItem from './RenderItem';
+import { styled } from "@mui/material/styles";
+
+export default function LabelBottomNavigation() {
+  const [value, setValue] = React.useState('');
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  const BottomNavigationAction = styled(MuiBottomNavigationAction)(`
+  &.Mui-selected {
+    color: red;
+  }
+`);
+
+  return (
+    <div>
+   <RenderItem valnav={value}> </RenderItem>
+    <BottomNavigation sx={{ width: 1 }} value={value} onChange={handleChange}>
+      <BottomNavigationAction 
+        label="World Cloud" 
+        value="Word Cloud"
+        icon={<CloudQueueIcon />}
+      />
+      <BottomNavigationAction 
+        label="Criteria Plotter"
+        value="Criteria Plotter"
+        icon={<BarChartIcon />}
+      />
+      <BottomNavigationAction
+        label="Time Spent"
+        value="Time Spent"
+        icon={<HourglassEmptyIcon />}
+      />
+      <BottomNavigationAction label="Written Reviews" value="Written Reviews" icon={<ArticleIcon />} />
+    </BottomNavigation>
+    </div>
+  );
+}
---
+++ src/RenderItem.js
@@ -0,0 +1,7 @@
+
+
+export default function RenderItem (props) {
+    return (
+        <h1> Ok, currently we have {props.valnav} </h1>
+    )
+}
